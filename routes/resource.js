bd24225215c571853c5ea8f00d5f1cbec5811f15
preethi54 <je.preethi@gmail.com>
2021-11-10 20:38:40 -0600

    updated part 5

--- routes/resource.js
+++ routes/resource.js
@@ -8,14 +8,14 @@ var bird_controller = require('../controllers/bird');
 router.get('/', api_controller.api);
 /// BIRD ROUTES ///
 // POST request for creating a birds.
-router.post('/resource/birds', bird_controller.bird_create_post);
+router.post('/birds', bird_controller.bird_create_post);
 // DELETE request to delete birds.
-router.delete('/resource/birds/:id', bird_controller.bird_delete);
+router.delete('/birds/:id', bird_controller.bird_delete);
 // PUT request to update birds.
-router.put('/resource/birds/:id', bird_controller.bird_update_put);
+router.put('/birds/:id', bird_controller.bird_update_put);
 // GET request for one birds.
-router.get('/resource/birds/:id', bird_controller.bird_detail);
+router.get('/birds/:id', bird_controller.bird_detail);
 // GET request for list of all Costume items.
-router.get('/resource/birds', bird_controller.bird_list);
+router.get('/birds', bird_controller.bird_list);
 module.exports = router;
 //
